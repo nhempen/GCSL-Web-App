0b4cda7969a7aa23b2d4f45b1879467cd8bf1b5e
nhempen <nhempen@outlook.com>
2021-11-28 14:26:43 -0500

    minor

--- src/components/NavBarElements.js
+++ src/components/NavBarElements.js
@@ -7,7 +7,6 @@ export const Nav = styled.nav`
   height: 85px;
   display: flex;
   justify-content: space-between;
-  padding: 0.2rem calc((100vw - 1000px) / 2);
   z-index: 12;
   /* Third Nav */
   /* justify-content: flex-start; */
